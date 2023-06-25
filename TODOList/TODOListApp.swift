8ab1ad2df8d805a93a52381037db44f1576c56a1
Rajesh sharma <rajesh.rajesh@publicissapient.com>
2023-06-25 17:22:59 +0530

    Initial Commit

---
+++ TODOList/TODOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListApp.swift
+//  TODOList
+//
+//  Created by Rajesh Rajesh on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TODOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
